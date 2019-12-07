db5d88e0ad0b902ded93d41916641cfac8ba4a8b
LanTenDragon <pantheonia1@gmail.com>
2019-12-07 23:36:55 +0800

    Update WebApp side of things

--- WebApplication2/Default.aspx.cs
+++ WebApplication2/Default.aspx.cs
@@ -1,4 +1,6 @@
-﻿using jsreport.Client;
+﻿using jsreport.Binary;
+using jsreport.Client;
+using jsreport.Local;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -9,18 +11,15 @@ namespace WebApplication2
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
-        ReportingService rs;
-
         protected void Page_Load(object sender, EventArgs e)
         {
-            rs = new ReportingService("http://localhost:5488");
         }
 
-        public class CompanyModel
+        public class DetailsModel
         {
-            public string Name { get; set; }
-            public string Road { get; set; }
-            public string Country { get; set; }
+            public long year { get; set; }
+            public int month { get; set; }
+            public string person { get; set; }
         }
 
         public class ItemModel
@@ -28,44 +27,68 @@ namespace WebApplication2
             public string Name { get; set; }
             public long Price { get; set; }
         }
+
+        public class PayModel
+        {
+            public string Name { get; set; }
+            public long Price { get; set; }
+        } 
         protected async void Button1_Click(object sender, EventArgs e)
         {
-            var report3 = await rs.RenderByNameAsync("Invoice", new
+            var rs = new LocalReporting()
+                        .UseBinary(JsReportBinary.GetBinary())
+                        .Configure(cfg => cfg.FileSystemStore().BaseUrlAsWorkingDirectory())
+                        .AsUtility()
+                        .Create();
+
+            var report3 = await rs.RenderByNameAsync("Payslip", new
             {
-                Number = "123",
-                Seller = new CompanyModel()
+                details = new List<DetailsModel>()
                 {
-                    Name = "Next Step Webs, Inc.",
-                    Road = "12345 Sunny Road",
-                    Country = "Sunnyville, TX 12345"
-                },
-                Buyer = new CompanyModel()
-                {
-                    Name = "Acme Corp.",
-                    Road = "16 Johnson Road",
-                    Country = "Paris, France 8060"
-                },
-                Items = new List<ItemModel>()
+                    new DetailsModel()
                     {
-                        new ItemModel()
-                        {
-                            Name = "Website design",
-                            Price = 300
-                        },
-                        new ItemModel()
-                        {
-                            Name = "Implementing specific components",
-                            Price = 600
-                        },
-                        new ItemModel()
-                        {
-                            Name = "Maintenance and support",
-                            Price = 150
-                        }
+                        person = "Dragon",
+                        year = 2017,
+                        month = 1
                     }
+                },
+                deductions = new List<PayModel>()
+                {
+                    new PayModel()
+                    {
+                        Name = "Unpaid Leave",
+                        Price = 300
+                    },
+                    new PayModel()
+                    {
+                        Name = "Social Security",
+                        Price = 400
+                    },
+                },
+                additions = new List<PayModel>()
+                {
+                    new PayModel()
+                    {
+                        Name = "Petrol Claim",
+                        Price = 0
+                    },
+                    new PayModel()
+                    {
+                        Name = "Monthly Allowance",
+                        Price = 400
+                    }
+                },
+                basepay = new List<PayModel>()
+                {
+                    new PayModel()
+                    {
+                        Name = "Base",
+                        Price = 2100
+                    }
+                }
             });
 
-            using (var fs = File.Create("\\Reports\\out.pdf"))
+            using (var fs = File.Create("C:\\Users\\Dragon\\source\\repos\\WebApplication2\\WebApplication2\\Reports\\out.pdf"))
             {
                 report3.Content.CopyTo(fs);
             }
