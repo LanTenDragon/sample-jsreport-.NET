bdb0cdf111e6769de6667e59b7b9bdb93d41da7d
LanTenDragon <pantheonia1@gmail.com>
2019-12-07 23:29:34 +0800

    Add Console Project for JSReport

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using jsreport.Binary;
+using jsreport.Local;
+using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var rs = new LocalReporting()
+                         .UseBinary(JsReportBinary.GetBinary())
+                         .KillRunningJsReportProcesses()
+                         .RunInDirectory(Path.Combine(Directory.GetCurrentDirectory(), "jsreport"))
+                         .Configure(cfg => cfg.CreateSamples()
+                            .FileSystemStore())
+                         .AsWebServer()
+                         .RedirectOutputToConsole()
+                         .Create();
+
+            rs.StartAsync().Wait();
+
+            Process.Start(new ProcessStartInfo("cmd", $"/c start http://localhost:5488"));
+
+            Console.ReadKey();
+
+            rs.KillAsync().Wait();
+        }
+    }
+}
